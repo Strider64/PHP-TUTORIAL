23dce4dd84e2d5dc9cbcd0200c2c23bd70d2afd6
John Pepp <jrpepp@pepster.com>
2017-10-06 13:15:24 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+  <?php
+  
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  define('DATABASE_HOST', 'local_host_name');
+  define('DATABASE_NAME', 'myCMS');
+  define('DATABASE_USERNAME', 'username');
+  define('DATABASE_PASSWORD', 'password');
+  define('DATABASE_TABLE', 'myBlog');
