294160dfd6570dd647ce268b8ab90aeece27dde3
John Pepp <jrpepp@pepster.com>
2017-10-07 09:43:00 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,9 @@
   
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
+
+  date_default_timezone_set('America/Detroit');
+
   define('DATABASE_HOST', 'local_host_name');
   define('DATABASE_NAME', 'myCMS');
   define('DATABASE_USERNAME', 'username');
